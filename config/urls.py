71a4d717e2500c9e782b2aa65776a3b32fc59288
Mumtaz dev <162595452+uzmumtazdev@users.noreply.github.com>
2024-06-13 21:42:18 +0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import staticfiles_urlpatterns
+from django.contrib.staticfiles.urls import staticfiles
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +11,6 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
+urlpatterns += staticfiles_urlpatterns()
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
