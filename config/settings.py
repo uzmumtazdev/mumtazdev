079cdfa64b89708bcd060d2c3383fddcdd142456
uzmumtazdev <arziqulovtv@gmail.com>
2024-06-15 06:06:36 +0500

    sad

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     "crispy_forms",
     "crispy_bootstrap5",
     'ckeditor',
